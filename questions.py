83e4d6bd28a8686336bea6ac4d93d1297fd2e003
pedrocapoccetti <pedrocapolostilos@gmail.com>
2026-03-25 16:07:58 -0300

    Delete questions.py

--- questions.py
+++
@@ -1,7 +0,0 @@
-# Punto 3: Programa que solicita un número y muestra su tabla de multiplicar del 1 al 10
-numero = int(input("Ingresá un número para ver su tabla de multiplicar: "))
-
-print(f"\nTabla del {numero}:")
-for i in range(1, 11):
-    resultado = numero * i
-    print(f"{numero} x {i} = {resultado}")
